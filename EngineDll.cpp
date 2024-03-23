9caf19bccea7634a0375b0ca1b79dd0051548cbd
Teo7297 <teocava7297@gmail.com>
2024-03-23 10:21:03 +0100

    start

---
+++ EngineDll.cpp
@@ -0,0 +1,8 @@
+#include "EngineDll.h"
+#include <iostream>
+
+// Entry point function
+void engineEntryPoint()
+{
+    std::cout << "Engine DLL entry point called." << std::endl;
+}
---
+++ EngineDllTest.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "EngineDll.h"
+
+int main(int argc, char const *argv[])
+{
+    engineEntryPoint();
+    return 0;
+}
